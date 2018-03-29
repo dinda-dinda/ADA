8805bc9ed354b4f4b931a9913003b68f01ac70b6
dinda-dinda <dindadindaart@gmail.com>
2018-03-29 18:12:59 -0300

    ProfileCard final

---
+++ Practicas/15_logica-0328/js/index.js
@@ -0,0 +1,30 @@
+/*Tomar dos variables, si ambas son mayores de 10, entonces los sumo, sino los multiplico*/
+
+x = 5;
+y = 17;
+z = 0;
+
+if (x > 10 || y > 10) {
+    z = x + y; /*Ejecuto la suma solo si ambas variables cumplen*/
+}
+/*else {
+   z = x * y; /*Ejecuto si al menos una es menor que 10*/
+
+
+console.log("El resultado es:" + z);
+/*Concateno la cadena de caracteres y la suma*/
+
+/*Mostrar mensaje de bienvenida incluyendo nombre y apellido 
+Solo si coinciden con los valores "Belen" y "Alegre"*/
+
+nombre = "Belen";
+apellido = "Alegre";
+bienvenida ="";
+console.log (bienvenida);
+
+if (nombre == "Belen" && apellido == "Alegre") {
+    bienvenida = ("Bienvenida " + nombre + " " + apellido + "!");
+    console.log(bienvenida);
+}
+
+    console.log(bienvenida);
